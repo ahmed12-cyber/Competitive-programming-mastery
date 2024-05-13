e6d2428980424289becdf1f66c7411b9e9a9ae27
Ahmed Ibn Rediwan <140769450+ahmed12-cyber@users.noreply.github.com>
2024-05-13 14:56:06 +0300

    Update 9. Palindrome Number.py

--- 9. Palindrome Number.py
+++ 9. Palindrome Number.py
@@ -4,10 +4,9 @@ class Solution(object):
         :type x: int
         :rtype: bool
         """
-        # Convert integer to string
+        
         str_x = str(x)
         
-        # Check if the string is equal to its reverse
         return str_x == str_x[::-1]
 
 
